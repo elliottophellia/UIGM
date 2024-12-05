082544f3b477581144b31787121a0077292d52d9
Reidho Satria <me@rei.my.id>
2024-12-05 17:39:55 +0700

    Create struc.cpp

---
+++ ALPRO2024/Tugas_10/struc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    char nama[25];
+    char npm[10];
+    float ipk;
+};
+
+int main() {3
+    Mahasiswa mhs[5];
+    
+    for(int i = 0; i < 5; i++) {
+        cout << "\nData Mahasiswa ke-" << i+1 << endl;
+        cout << "Masukkan nama : "; cin >> mhs[i].nama;
+        cout << "Masukkan NPM : "; cin >> mhs[i].npm;
+        cout << "Masukkan IPK : "; cin >> mhs[i].ipk;
+    }
+    
+    cout << "\n****OUTPUT****" << endl;
+    for(int i = 0; i < 5; i++) {
+        cout << "\nData Mahasiswa ke-" << i+1 << endl;
+        cout << "Nama : " << mhs[i].nama << endl;
+        cout << "NPM  : " << mhs[i].npm << endl;
+        cout << "IPK  : " << mhs[i].ipk << endl;
+    }
+    
+    return 0;
+}
